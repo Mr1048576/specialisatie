86295a31a2f56c63db673b5ace377600d8806628
Mr1048576 <wouter_44@hotmail.com>
2014-10-02 23:47:00 +0200

    Added first Tutorial Website
    If you can even call it a website, these are the absolute basics (but
    thorough understanding of the basics will be necessary to fully grasp
    all the extras)

---
+++ Laraval Startings/js/app.js
@@ -0,0 +1,2 @@
+var app = angular.module("TutorialApp", []);
+//angular.module takes 2 arguments; first is name of module (this is for the one in html page), second is array of dependencies -> used for outside references
---
+++ Laraval Startings/js/tutorialcontroller.js
@@ -0,0 +1,10 @@
+//name of file doesn't really matter (duh ;) )
+app.controller("TutorialController", function ($scope){
+	/*$scope.info = "I know what scope does now! (I hope :p)";*/
+	
+});
+//= controller setup; ready to go
+//note! no var at beginning for controller
+//second argument is a function, this is where we're going to hold everything for the controller
+// scope is where to define controller variables, in turn will make them available in the div from the index.html
+// index & controller don't know about each other, but they both know about scope
